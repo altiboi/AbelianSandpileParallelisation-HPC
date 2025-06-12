6e44ffcb08c8ae418c0ad3f9b62e62f06c797a1c
altiboi <altiboi1821@gmail.com>
2025-06-12 13:21:49 +0200

    OMP final

--- OMP/sandpile_omp.c
+++ OMP/sandpile_omp.c
@@ -2,79 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-#include <png.h>
 #include <omp.h>
-
-void write_png(const char *filename, int **grid, int N, int M)
-{
-    FILE *fp = fopen(filename, "wb");
-    if (!fp)
-    {
-        perror("fopen");
-        exit(EXIT_FAILURE);
-    }
-
-    png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    png_infop info = png_create_info_struct(png);
-    if (!png || !info)
-    {
-        fprintf(stderr, "PNG creation failed\n");
-        exit(EXIT_FAILURE);
-    }
-
-    if (setjmp(png_jmpbuf(png)))
-    {
-        fprintf(stderr, "PNG error during creation\n");
-        exit(EXIT_FAILURE);
-    }
-
-    png_init_io(png, fp);
-    png_set_IHDR(png, info, M, N, 8, PNG_COLOR_TYPE_RGB,
-                 PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
-    png_write_info(png, info);
-
-    png_bytep row = malloc(3 * M);
-    for (int y = 0; y < N; y++)
-    {
-        for (int x = 0; x < M; x++)
-        {
-            int val = grid[y][x];
-            switch (val)
-            {
-            case 0:
-                row[3 * x + 0] = 0;
-                row[3 * x + 1] = 0;
-                row[3 * x + 2] = 0;
-                break;
-            case 1:
-                row[3 * x + 0] = 0;
-                row[3 * x + 1] = 255;
-                row[3 * x + 2] = 0;
-                break;
-            case 2:
-                row[3 * x + 0] = 0;
-                row[3 * x + 1] = 0;
-                row[3 * x + 2] = 255;
-                break;
-            case 3:
-                row[3 * x + 0] = 255;
-                row[3 * x + 1] = 0;
-                row[3 * x + 2] = 0;
-                break;
-            default:
-                row[3 * x + 0] = 255;
-                row[3 * x + 1] = 255;
-                row[3 * x + 2] = 255;
-                break;
-            }
-        }
-        png_write_row(png, row);
-    }
-    free(row);
-    png_write_end(png, NULL);
-    fclose(fp);
-    png_destroy_write_struct(&png, &info);
-}
 
 char *generate_output_filename(const char *input_filename)
 {
@@ -94,14 +22,14 @@ int main(int argc, char *argv[])
 {
     if (argc != 5)
     {
-        fprintf(stderr, "Usage: %s N M input.txt image.png\n", argv[0]);
+        fprintf(stderr, "Usage: %s N M input.txt output.txt\n", argv[0]);
         return EXIT_FAILURE;
     }
 
     int N = atoi(argv[1]);
     int M = atoi(argv[2]);
     const char *input_filename = argv[3];
-    const char *image_filename = argv[4];
+    const char *output_filename = argv[4];
 
     FILE *input = fopen(input_filename, "r");
     if (!input)
@@ -110,7 +38,6 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    char *output_filename = generate_output_filename(input_filename);
     FILE *output = fopen(output_filename, "w");
     if (!output)
     {
@@ -137,7 +64,7 @@ int main(int argc, char *argv[])
     {
         changed = false;
 
-        // Red phase: Update cells where (i + j) % 2 == 0
+        // Red phase: Updates cells where (i + j) % 2 == 0
         #pragma omp parallel for collapse(2) reduction(||:changed)
         for (int i = 0; i < N; i++)
         {
@@ -175,7 +102,7 @@ int main(int argc, char *argv[])
             }
         }
 
-        // Black phase: Update cells where (i + j) % 2 == 1
+        // Black phase: Updates cells where (i + j) % 2 == 1
         #pragma omp parallel for collapse(2) reduction(||:changed)
         for (int i = 0; i < N; i++)
         {
@@ -215,7 +142,7 @@ int main(int argc, char *argv[])
     }
 
     double end_time = omp_get_wtime();
-    printf("OpenMP Red-Black time: %f seconds\n", end_time - start_time);
+    printf("OpenMP time: %f seconds\n", end_time - start_time);
 
     for (int i = 0; i < N; i++)
     {
@@ -227,14 +154,12 @@ int main(int argc, char *argv[])
     }
 
     fclose(output);
-    write_png(image_filename, grid, N, M);
 
     for (int i = 0; i < N; i++)
     {
         free(grid[i]);
     }
     free(grid);
-    free(output_filename);
 
     return EXIT_SUCCESS;
 }
